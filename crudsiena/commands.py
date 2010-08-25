b02dfa9d0a75163d6748844155e85393b3e9916b
pascal.voitot.dev <pascal.voitot.dev@4015059e-e4f8-e183-2350-cf514d93e0ed>
2010-08-25 20:19:14 +0000

    +added crudsiena:ov extract commands

--- crudsiena/commands.py
+++ crudsiena/commands.py
@@ -1,15 +1,36 @@
-# Here you can create play commands that are specific to the module
+# CRUD
 
-# Example below:
-# ~~~~
-if play_command == 'crudsiena:hello':
+# ~~~~~~~~~~~~~~~~~~~~~~ [crud:ov] Override a view
+if play_command == 'crudsiena:ov' or play_command == 'crudsiena:override':
 	try:
-		print "~ Hello from crudsiena"
-		sys.exit(0)
+		optlist, args = getopt.getopt(remaining_args, 't:', ['css','layout','template='])
+		for o, a in optlist:
+			if o in ('-t', '--template'):
+				c = a.split('/')[0]
+				t = a.split('/')[1]
+				override('app/views/CRUD/%s.html' % t, 'app/views/%s/%s.html' % (c, t))
+				print "~ "
+				sys.exit(0)
+			
+			if o == '--layout':
+				override('app/views/CRUD/layout.html', 'app/views/CRUD/layout.html')
+				print "~ "
+				sys.exit(0)
+				
+			if o == '--css':
+				override('public/stylesheets/crud.css', 'public/stylesheets/crud.css')
+				print "~ "
+				sys.exit(0)
 				
 	except getopt.GetoptError, err:
 		print "~ %s" % str(err)
 		print "~ "
 		sys.exit(-1)
+	
+	print "~ Specify the template to override, ex : -t Users/list" 
+	print "~ "
+	print "~ Use --css to override the CRUD css" 
+	print "~ Use --layout to override the CRUD layout" 
+	print "~ "
 		
 	sys.exit(0)
