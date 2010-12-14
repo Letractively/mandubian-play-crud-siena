9442dd32ce8bbe1cc2343d76b064adc9e04fc3ff
pascal.voitot.dev <pascal.voitot.dev@4015059e-e4f8-e183-2350-cf514d93e0ed>
2010-12-14 22:21:07 +0000

    tagging version 1.3

---
+++ tags/1.3/crudsiena/src/play/modules/crudsiena/SienaUtils.java
@@ -0,0 +1,47 @@
+package play.modules.crudsiena;
+
+import java.lang.reflect.Field;
+
+import play.exceptions.UnexpectedException;
+import siena.Id;
+
+public class SienaUtils {
+    // More utils
+    public static Object findKey(Object entity) {
+        try {
+            Class c = entity.getClass();
+            while (!c.equals(Object.class)) {
+                for (Field field : c.getDeclaredFields()) {
+                    if (field.isAnnotationPresent(Id.class)) {
+                        field.setAccessible(true);
+                        return field.get(entity);
+                    }
+                }
+                c = c.getSuperclass();
+            }
+        } catch (Exception e) {
+            throw new UnexpectedException("Error while determining the object @Id for an object of type " + entity.getClass());
+        }
+        return null;
+    }    
+    
+ 	public static Class findKeyType(Class c) {
+        try {
+            while (!c.equals(Object.class)) {
+                for (Field field : c.getDeclaredFields()) {
+                    if (field.isAnnotationPresent(Id.class)) {
+                        field.setAccessible(true);
+                        return field.getType();
+                    }
+                }
+                c = c.getSuperclass();
+            }
+        } catch (Exception e) {
+            throw new UnexpectedException("Error while determining the object @Id for an object of type " + c);
+        }
+        return null;
+    }
+ 	
+
+
+}
