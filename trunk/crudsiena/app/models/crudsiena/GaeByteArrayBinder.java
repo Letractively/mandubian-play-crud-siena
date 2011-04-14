ba935b297480e18d2eec187e4177a882ef8dace0
pascal.voitot.dev@gmail.com <pascal.voitot.dev@gmail.com@4015059e-e4f8-e183-2350-cf514d93e0ed>
2011-04-14 08:46:06 +0000

    * added Play v1.2 support (no more compatible with v<1.2)

--- trunk/crudsiena/app/models/crudsiena/GaeByteArrayBinder.java
+++ trunk/crudsiena/app/models/crudsiena/GaeByteArrayBinder.java
@@ -1,22 +1,18 @@
 package models.crudsiena;
 
-import play.data.binding.TypeBinder;
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
 import java.util.List;
-
-import com.google.appengine.api.datastore.Blob;
 
 import play.data.Upload;
 import play.data.binding.Binder;
-import play.db.Model;
-import play.exceptions.UnexpectedException;
+import play.data.binding.TypeBinder;
 import play.mvc.Http.Request;
-import play.mvc.Scope.Params;
 
 public class GaeByteArrayBinder implements TypeBinder<byte[]> {
 
 	@SuppressWarnings("unchecked")
-    public Object bind(String name, Annotation[] annotations, String value, Class actualClass) {
+    public Object bind(String name, Annotation[] annotations, String value, Class actualClass, Type genericType) throws Exception {
         List<Upload> uploads = (List<Upload>) Request.current().args.get("__UPLOADS");
         for(Upload upload : uploads) {
             if(upload.getFieldName().equals(value)) {
