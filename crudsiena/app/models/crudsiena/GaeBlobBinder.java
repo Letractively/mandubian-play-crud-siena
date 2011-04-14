22af5b1d77426c55d1b31fddbc0790c22eef5ebf
pascal.voitot.dev <pascal.voitot.dev@gmail.com>
2011-04-14 08:46:06 +0000

    * added Play v1.2 support (no more compatible with v<1.2)

--- crudsiena/app/models/crudsiena/GaeBlobBinder.java
+++ crudsiena/app/models/crudsiena/GaeBlobBinder.java
@@ -1,19 +1,21 @@
 package models.crudsiena;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
 import java.util.List;
 
-import com.google.appengine.api.datastore.Blob;
-import com.google.gson.*;
-
 import play.data.Upload;
-import play.data.binding.*;
+import play.data.binding.Binder;
+import play.data.binding.Global;
+import play.data.binding.TypeBinder;
 import play.mvc.Http.Request;
+
+import com.google.appengine.api.datastore.Blob;
 
 @Global
 public class GaeBlobBinder implements TypeBinder<Blob> {
 
-    public Object bind(String name, Annotation[] antns, String value, Class type) throws Exception {
+    public Object bind(String name, Annotation[] annotations, String value, Class actualClass, Type genericType) throws Exception {
     	List<Upload> uploads = (List<Upload>)Request.current().args.get("__UPLOADS");
         for(Upload upload : uploads) {
             if(upload.getFieldName().equals(value)) {
@@ -25,4 +27,5 @@ public class GaeBlobBinder implements TypeBinder<Blob> {
         }
         return Binder.MISSING;
     }
+
 }
